0b3fd873bb94a90a19826189100d8fb3a118c07b
Fabio de Matos <117763868+FabioMatos-Dev@users.noreply.github.com>
2023-10-05 17:02:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/////TEXT ANIMATION//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let words = document.querySelectorAll(".word");
-words.forEach((word)=>){
+words.forEach((word)=>{
     let letters = word.textContent.split("");
     word.textContent="";
-    letters.forEach(letter)=>{
+    letters.forEach((letter)=>{
         let span = document.createElement("span");
         span.textContent = letter;
         span.className = "letter";
@@ -12,7 +14,7 @@ words.forEach((word)=>){
 
 let currentWordIndex = 0;
 let maxWordIndex = words.length -1;
-words[currentWordIndex].computedStyleMap.opacity = "1";
+words[currentWordIndex].style.opacity = "1";
 
 let changeText = ()=>{
     let currentWord = words[currentWordIndex];
@@ -22,7 +24,7 @@ let changeText = ()=>{
         setTimeout(()=>{
             letter.className = "letter out";
         },i*80);
-    })
+    });
     nextWord.style.opacity="1";
     Array.from(nextWord.children).forEach((letter,i)=>{
         letter.className = "letter behind";
@@ -30,9 +32,95 @@ let changeText = ()=>{
             letter.className = "letter in";
         },340 + i * 80);
     });
-    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1];
-}
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
 
 changeText();
 setInterval(changeText,3000)
-//Script feito corretamente, mas nao esta funcionando!!!!!!!
+
+///CIRCLE SKILL///////////////////////////////////////////////////////////////////////////////////////////////////////
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for(let i = 0; i < dots; i++){
+        points += '<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>';
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
+
+
+//MIX ITUP PORTFOLIO SECTION///////////////////////////////////////////////////////////////////////////////////////
+
+var mixer = mixitup(".portfolio-gallery");
+
+
+//ACTIVE MENU////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+//STICKY NAVBAR////////////////////////////////////////////////////////////////////////////////////////////////
+
+const header = document.querySelectorAll("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
+
+
+//TOGGLE ICON NAVBAR////////////////////////////////////////////////////////////////////////////////////////////////
+
+let menuIcon = document.querySelectorAll("#menu-icon");
+let navlist = document.querySelectorAll(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
+//PARALLAX//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
